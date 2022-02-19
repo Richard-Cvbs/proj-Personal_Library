0b2a7791d8d70b7f1b05b616557180aff6a12ad5
Diego Felix <dt196397@gmail.com>
2022-02-19 13:37:19 -0600

    +Event Listeners + Add&Remove Elements+Toggle Read

--- app.js
+++ app.js
@@ -3,29 +3,99 @@ function Book(title,author,pages,read){
     this.author = author;
     this.pages = pages
     this.read = read
-    this.info = function (){
-        if (read){
-            readValue = "have already read"
-        } else {
-            readValue = "have not read yet"
-        }
-        return `${title} by ${author}, ${pages} pages ${readValue}`
+    this.readValue = function (){
+        if (this.read){
+           return "Already Read!"
+        } return "Haven't Read Yet"
+    }
+    this.toggleRead = function(){
+        if (this.read===true){
+         return this.read = false
+        } return this.read = true
     }
 }
-const Book1 = new Book("Lord of the rings", "J.R. Tolkien", "10008000", false)
-const Book2 = new Book("Random book 3", "author 3", "8000", true)
-const Book3 = new Book("Lrandom book 4", "author 4", "86", true)
-const Book4 = new Book("rebel book", "unknown", "683", false)
-
-
+const Book1 = new Book("Pride and Prejudice", "Jane Austen", "334", true)
 const myLibrary = [];
+function addItem(Book){
+    const container = document.querySelector(".library-cont")
+    const newBook = document.createElement("div")
+    newBook.className = "book-item"
+    /* Create and style text */
+    const bookText = document.createElement("div")
+    bookText.className = "book-text"
 
+    const bookTitle = document.createElement("div")
+    bookTitle.className = "book-title"
+    bookTitle.textContent = Book.title
+    bookText.appendChild(bookTitle)
+
+    const bookAuthor = document.createElement("div")
+    bookAuthor.className = "book-autor"
+    bookAuthor.textContent = Book.author
+    bookText.appendChild(bookAuthor)
+
+    const bookPageCount = document.createElement("div")
+    bookPageCount.className = "book-pageCount"
+    bookPageCount.textContent = `${Book.pages} Pages`
+    bookText.appendChild(bookPageCount)
+
+    const bookRead = document.createElement("div")
+    bookRead.className = "book-read"
+    bookRead.textContent = Book.readValue()
+    bookText.appendChild(bookRead)
+
+    newBook.appendChild(bookText)
+
+    /*Create and style buttons */
+    const bttnContainer = document.createElement('div')
+    bttnContainer.className = "button-cont"
+
+    const toggleReadBttn = document.createElement('button')
+    toggleReadBttn.className = "book-toggleRead"
+    toggleReadBttn.addEventListener('click', e=>{
+        toggleReadAct(e.target,Book)
+    })
+
+    const toggleReadImg = document.createElement("img")
+    toggleReadImg.setAttribute("src", "/assets/menu_book_black_36dp.svg")
+    toggleReadBttn.appendChild(toggleReadImg)
+
+    bttnContainer.appendChild(toggleReadBttn)
+
+    const removeBookButtn = document.createElement('button')
+    removeBookButtn.className = "book-remove"
+    removeBookButtn.addEventListener('click',e =>{
+        removeBook(e.target,Book)
+    })
+    
+    const removeBookImg = document.createElement('img')
+    removeBookImg.setAttribute("src", "/assets/remove_black.svg")
+    removeBookButtn.appendChild(removeBookImg)
+
+    bttnContainer.appendChild(removeBookButtn)
+
+    newBook.appendChild(bttnContainer)
+
+    container.appendChild(newBook)
+}
 
 function addBookToLibrary(Book){
-    myLibrary.push(Book.info())
+    myLibrary.push(Book)
+    addItem(Book)
+    return
 }
-addBookToLibrary(Book1)
-addBookToLibrary(Book2)
-addBookToLibrary(Book3)
-addBookToLibrary(Book4)
-
+function removeBook(e,Book){
+    let indexOfBook = myLibrary.indexOf(Book)
+    if (indexOfBook > -1) {
+        myLibrary.splice(indexOfBook, 1);
+      }
+    let thisBook = e.closest('.book-item')
+    thisBook.parentNode.removeChild(thisBook)
+}
+function toggleReadAct(e,Book){
+    Book.toggleRead()
+    Book.read
+    let thisBook = e.closest('.book-item')
+    let readNode = thisBook.querySelector('.book-read')
+    readNode.textContent = Book.readValue()
+}
